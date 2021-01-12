bdafe8e157ab5eb124b5efbaad45bea77df343b7
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-01-12 18:54:40 +0200

    max-height animation

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -4,3 +4,29 @@ const MIN_PASSWORD_LENGTH = 8;
 document.addEventListener("click", function (e) {
     if (e.target.href === "#" || e.target.closest(`[href="#"]`)) e.preventDefault();
 });
+//----------------------------------------------------------------------------------------------------------------------
+// раскрывающиеся по высоте блоки
+function maxHeightData() {
+    return {
+        show: false,
+        maxHeightResize: {
+            ["@resize.window"]() {
+                if (this.show) {
+                    this.$refs.max_h_outer.style.maxHeight = this.$refs.max_h_inner.clientHeight + "px";
+                } else {
+                    this.$refs.max_h_outer.style.maxHeight = "0";
+                }
+            },
+        },
+    };
+}
+
+function maxHeightClick() {
+    this.$watch("show", value => {
+        if (value) {
+            this.$refs.max_h_outer.style.maxHeight = this.$refs.max_h_inner.clientHeight + "px";
+        } else {
+            this.$refs.max_h_outer.style.maxHeight = "0";
+        }
+    });
+}
